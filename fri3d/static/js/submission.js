935cf84365001f7dbc306e843783511b790d52d2
Jef Van den broeck <jef.vdb@gmail.com>
2017-09-30 01:33:54 +0200

    use a standardish bootstrap validation lib (need safari test), provide a name for all form fields (matching the Angular version's nomenclature, so form.serialize() works)

--- fri3d/static/js/submission.js
+++ fri3d/static/js/submission.js
@@ -1,14 +1,24 @@
 // submission (validation) support
+// vim: set expandtab sw=2 ts=2 :
+
+// vert pixel offset of highlighted bad inputs,
+// skip past our static header
+$.fn.validator.Constructor.FOCUS_OFFSET = 100;
+
+$('#cfp_form').validator().on('submit', function(e) {
+  if (e.isDefaultPrevented()) {
+    // validation found & highlighted an error
+    return false;
+  }
+  e.preventDefault();
+  window.submission.submit();
+});
 
 window.submission = {};
 
 (function(submission) {
 
   submission.submit = function submit() {
-    var form = $('#form_cfp')[0];
-    if (!form.checkValidity()) {
-      form.reportValidity();
-    }
     // backend things
   }
 
