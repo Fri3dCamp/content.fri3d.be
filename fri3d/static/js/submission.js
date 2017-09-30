214e6f0221a869d005ba513d832cd6ad8313d7e0
Jef Van den broeck <jef@codewerken.be>
2017-09-30 15:29:32 +0200

    add a custom validator enforcing data-max-chars (element doesn't need to be marked required)

--- fri3d/static/js/submission.js
+++ fri3d/static/js/submission.js
@@ -1,27 +1,44 @@
 // submission (validation) support
 // vim: set expandtab sw=2 ts=2 :
 
-// vert pixel offset of highlighted bad inputs,
-// skip past our static header
-$.fn.validator.Constructor.FOCUS_OFFSET = 100;
+$(document).ready(function() {
+  $('#cfp_form').validator(custom={
+    custom: {
+      'max-chars': function($el) {
+        var max = $el.data('max-chars');
+        console.log($el + " " + max + " " + $el.val().length);
+        if ($el.val().length > max) {
+          return "Te lang!";
+        }
+      }
+    },
+    errors: {
+      'max-chars': "Te lang!",
+    }
+  });
 
-$('#cfp_form').validator().on('submit', function(e) {
-  if (e.isDefaultPrevented()) {
-    // validation found & highlighted an error
-    return false;
-  }
-  e.preventDefault();
-  window.submission.submit();
-});
 
-// show/hide error message close to submit button
+  // vert pixel offset of highlighted bad inputs,
+  // skip past our static header
+  $.fn.validator.Constructor.FOCUS_OFFSET = 100;
 
-$("#cfp_form").validator().on("validated.bs.validator", function(e) {
-  if( $(this).data('bs.validator').hasErrors() ) {
-    $("#errors").show();
-  } else {
-    $("#errors").hide();
-  }
+  $('#cfp_form').validator().on('submit', function(e) {
+    if (e.isDefaultPrevented()) {
+      // validation found & highlighted an error
+      return false;
+    }
+    e.preventDefault();
+    window.submission.submit();
+  });
+  // show/hide error message close to submit button
+
+  $("#cfp_form").validator().on("validated.bs.validator", function(e) {
+    if( $(this).data('bs.validator').hasErrors() ) {
+      $("#errors").show();
+    } else {
+      $("#errors").hide();
+    }
+  });
 });
 
 // validate length of all textarea
