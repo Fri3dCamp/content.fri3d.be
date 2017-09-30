8cfd7def6bfa830c8b5877577a212063bf1ae934
Christophe VG <contact@christophe.vg>
2017-09-30 11:58:37 +0200

    added visual cue for limitation on textareas

--- fri3d/static/js/submission.js
+++ fri3d/static/js/submission.js
@@ -14,6 +14,28 @@ $('#cfp_form').validator().on('submit', function(e) {
   window.submission.submit();
 });
 
+// validate length of all textarea
+
+$( document ).ready(function() {
+  $("textarea").each(function(index) {
+    if( $(this).attr("data-max-chars") ) {
+      // generate label for "remaining" char count
+      var label = $( "<div />", { "class": "pull-right remaining" } );
+      $(this).after(label);
+      var max = parseInt($(this).data("max-chars"));
+      $(label).html(max + "/" + max);
+      $(this).keyup(function() {
+        var remaining = max - $(this).val().length;
+        $(label).html(remaining + "/" + max);
+        label.removeClass("invalid");
+        if(remaining < 0) { label.addClass("invalid"); }
+      });
+    }
+  });
+});
+
+// a stubbed submission module ...
+
 window.submission = {};
 
 (function(submission) {
